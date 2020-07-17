35e761dac72097e9c9a9fbccf8fb9f45e0b77244
Ashutosh Anand <ashutoshanand139@gmail.com>
2020-07-17 22:59:19 +0530

    Show more user friendly messages
    Minor bug fixes and added other destination path for flutter projects
    This is only for personal preference

--- automate.py
+++ automate.py
@@ -16,44 +16,62 @@ except:
 
 path = os.environ.get('MainProjectPath')          
 new_dir = path + foldername
+flutter_dir = path + "FlutterApps/" + foldername
 
 if foldername == "":
   print(">> Please use proper format.")
   print(">> Run \"create --help\" for instructions.")
 
-elif flag == "" or flag == "-gl" or flag == "-lg" or flag == "-pb":
+elif flag == "" or flag == "-gl" or flag == "-lg" or flag == "-pb" or flag == "-f":
   try:
-    os.mkdir(new_dir)
-    os.chdir(new_dir)
+    if flag != "-f":
+      os.mkdir(new_dir)
+      os.chdir(new_dir)
+    else:
+      os.mkdir(flutter_dir)
+      os.chdir(flutter_dir)
+
     token = os.environ.get('GithubToken')
     user = ghb(token).get_user()
     login = user.login
 
     if status == "" or status == "-pb":
-      repo = user.create_repo(foldername)
-      commands = [f'echo # {repo.name} >> README.md',
+      try:
+        repo = user.create_repo(foldername)
+        commands = [f'echo # {repo.name} >> README.md',
             'git init',
             f'git remote add origin https://github.com/{login}/{foldername}.git',
             'git add .',
             'git commit -m "Initial commit"',
             'git push -u origin master']
-      for c in commands:
-        os.system(c)
-      print(f'{foldername} created locally as well as an public repository on Github.')
-      os.system('code .')
+        for c in commands:
+          os.system(c)
+        print(f'{foldername} created locally as well as an public repository on Github.')
+
+      except:
+        print("Folder created locally but Github repository of the same name already exists!")  
+      
+      if flag != "-f":
+        os.system('code .')
 
     elif status == "-pr":
-      repo = user.create_repo(foldername, private=True)
-      commands = [f'echo # {repo.name} >> README.md',
-            'git init',
-            f'git remote add origin https://github.com/{login}/{foldername}.git',
-            'git add .',
-            'git commit -m "Initial commit"',
-            'git push -u origin master']
-      for c in commands:
-        os.system(c)
-      print(f'{foldername} created locally as well as an private repository on Github.')
-      os.system('code .')
+      try:
+        repo = user.create_repo(foldername, private=True)
+        commands = [f'echo # {repo.name} >> README.md',
+              'git init',
+              f'git remote add origin https://github.com/{login}/{foldername}.git',
+              'git add .',
+              'git commit -m "Initial commit"',
+              'git push -u origin master']
+        for c in commands:
+          os.system(c)
+        print(f'{foldername} created locally as well as an private repository on Github.')
+
+      except:
+        print("Folder created locally but Github repository of the same name already exists!")
+
+      if flag != "-f":
+        os.system('code .')
 
     else:
       print(">> Please use proper format.")
@@ -69,17 +87,23 @@ elif flag == "-pr":
     token = os.environ.get('GithubToken')
     user = ghb(token).get_user()
     login = user.login
-    repo = user.create_repo(foldername, private=True)
-    commands = [f'echo # {repo.name} >> README.md',
-          'git init',
-          f'git remote add origin https://github.com/{login}/{foldername}.git',
-          'git add .',
-          'git commit -m "Initial commit"',
-          'git push -u origin master']
-    for c in commands:
-      os.system(c)
-    print(f'{foldername} created locally as well as an public repository on Github.')
-    os.system('code .')  
+
+    try:
+      repo = user.create_repo(foldername, private=True)
+      commands = [f'echo # {repo.name} >> README.md',
+            'git init',
+            f'git remote add origin https://github.com/{login}/{foldername}.git',
+            'git add .',
+            'git commit -m "Initial commit"',
+            'git push -u origin master']
+      for c in commands:
+        os.system(c)
+      print(f'{foldername} created locally as well as an public repository on Github.')
+      os.system('code .') 
+
+    except:
+        print("Folder created locally but Github repository of the same name already exists!")   
+
   except:
     print("This foldername already exists.")    
 
@@ -103,14 +127,24 @@ elif flag == "-g":
     token = os.environ.get('GithubToken')
     user = ghb(token).get_user()
     login = user.login
-    repo = user.create_repo(foldername)
-    print(f'{foldername} public repository created on Github.')
+
+    try:
+      repo = user.create_repo(foldername)
+      print(f'{foldername} public repository created on Github.')
+    except:
+        print("Folder created locally but Github repository of the same name already exists!")
+
   elif status == "-pr":
     token = os.environ.get('GithubToken')
     user = ghb(token).get_user()
     login = user.login
-    repo = user.create_repo(foldername, private=True)
-    print(f'{foldername} private repository created on Github.')
+
+    try:
+      repo = user.create_repo(foldername, private=True)
+      print(f'{foldername} private repository created on Github.')
+    except:
+        print("Folder created locally but Github repository of the same name already exists!")
+
   else:
     print(">> Please use proper format.")
     print(">> Run \"create --help\" for instructions.")
@@ -119,4 +153,3 @@ else:
   print(">> Please use proper format.")
   print(">> Run \"create --help\" for instructions.")
   
-    
